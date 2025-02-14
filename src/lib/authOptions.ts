c1b682a3ac4dc043fa2862b7bb962861b3281610
Joseph Ngige <josephngige411@gmail.com>
2025-02-14 08:21:27 +0300

    resolved build errors 16

--- src/lib/authOptions.ts
+++ src/lib/authOptions.ts
@@ -1,4 +1,4 @@
-import { NextAuthOptions } from "next-auth";
+import { type NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
